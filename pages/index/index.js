e89a4cf1bf0cdf96753940c7d97289be7612fc5d
xuqingxi <3104792940@qq.com>
2018-08-26 21:37:37 +0800

    完善分类显示与日期显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,9 @@ import {axios} from '../../utils/util.js'
 Page({
   data: {
     imgUrls: [],
-    booktype:["前端开发","操作系统","移动开发","后端开发"],
-    fontEnd:[],
-    os:[],
-    mobile:[],
-    backEnd:[],
+    booktype:[],
+    bookcontent:[],
+    updatetime:[],
     autoplay: true,
     interval: 5000,
     duration: 1000,
@@ -19,11 +17,46 @@ Page({
       bookSize:4
     }
     axios.fetch('/category/books',params).then(res=>{
+      console.log(new Date().getDate())
+      let types = [];
+      let content=[];
+      let update = [];
+      res.data.forEach(item=>{
+        let timeList = []
+        item.books.forEach(m=>{
+      
+          let day = new Date(m.updateTime).getDate();
+          let month = new Date(m.updateTime).getMonth();
+          let year = new Date(m.updateTime).getFullYear();
+          let currentDay = new Date().getDate();
+          let currentMonth = new Date().getMonth();
+          let currentYear = new Date().getFullYear();
+          console.log(year)
+          console.log(currentYear)
+          if(year == currentYear){
+            if(month == currentMonth){
+              if(day == currentDay){
+                timeList.push('今天')
+              }else{
+                let dayDiffer =currentDay -day
+                timeList.push(dayDiffer+'天前')
+              }
+            }else{
+              let monthDiffer =currentMonth - month
+              timeList.push(monthDiffer+'月前')
+            }
+          }else{
+            timeList.push('一年前')
+          }
+        })
+        update.push(timeList)
+        content.push(item.books)
+        types.push(item.title)
+      })
       this.setData({
-        fontEnd:res.data[0].books,
-        os:res.data[1].books,
-        mobile: res.data[2].books,
-        backEnd: res.data[3].books
+        booktype:types,
+        bookcontent:content,
+        updatetime:update
       })
     })
     axios.fetch('/swiper').then(res=>{
